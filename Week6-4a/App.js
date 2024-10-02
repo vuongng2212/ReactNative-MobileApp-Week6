01449570fb36526a5a763b3a46f0a069338a287b
VuongNguyen <kudoyaung@gmail.com>
2024-10-03 01:10:43 +0700

    Update App.js flatlist

--- Week6-4a/App.js
+++ Week6-4a/App.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet, Button, View, Image } from 'react-native';
+import { Text, StyleSheet, Button, View, Image, FlatList } from 'react-native';
 import arrow_left from './assets/ant-design_arrow-left-outlined.png';
 import cart from './assets/bi_cart-check.png';
 import caNauLau from './assets/ca_nau_lau.png';
@@ -8,6 +8,62 @@ import doChoiMH from './assets/do_choi_dang_mo_hinh.png';
 import hieuLongConTre from './assets/hieu_long_con_tre.png';
 import lanhDaoGianDon from './assets/lanh_dao_gian_don.png';
 import rec1 from './assets/Rectangle1.png';
+
+const DATA = [
+  {
+    id: '1',
+    title: 'Ca nấu lẫu, nấu mì mini...',
+    img_url:caNauLau,
+    shop:'Shop DeVang'
+  },
+  {
+    id: '2',
+    title: '1KG KHÔ GÀ BƠ TỎI',
+    img_url:gaBoToi,
+    shop:'Shop LDTFood'
+  },
+  {
+    id: '3',
+    title: 'Xe cần cẩu đa năng',
+    img_url:xeCanCau,
+    shop:'Shop Thể giới đồ chơi'
+  },
+  {
+    id: '4',
+    title: 'Đồ chơi dạng mô hình',
+    img_url:doChoiMH,
+    shop:'Shop Thể giới đồ chơi'
+  },
+  {
+    id: '5',
+    title: 'Lãnh đạo giản đơn',
+    img_url:hieuLongConTre,
+    shop:'Shop Minh Long Book'
+  },
+  {
+    id: '6',
+    title: 'Hiểu lòng con trẻ',
+    img_url:lanhDaoGianDon,
+    shop:'Shop Minh Long Book'
+  },
+];
+
+
+const Item = ({title, img_url, shop}) => (
+  <View style={styles.content}>
+    <Image source={img_url} style={styles.IMGContent}/> 
+    <View style={{marginTop:15}}>
+      <Text>{title}</Text>
+      <Text style={{color:'#F31111'}}>{shop}</Text>
+    </View>
+    <View style={{marginTop:15}}>
+      <Image source={rec1}/>
+      <Text style={styles.viewChatRec}>
+        Chat
+      </Text>
+    </View>
+  </View>
+);
 
 export default function App() {
   return (
@@ -20,85 +76,14 @@ export default function App() {
       <View style={{margin:15}}>
         <Text>Bạn có thắc mắc với sản phẩm vừa xem đừng ngại chát với shop!</Text>
       </View>
+
       <View style={styles.content}>
-        <Image source={caNauLau} style={styles.IMGContent}/>
-        <View>
-          <Text>Ca nấu lẫu, nấu mì mini...</Text>
-          <Text style={{color:'#F31111'}}>Shop Devang</Text>
-        </View>
-        <View>
-          <Image source={rec1} style={styles.chatRec}/>
-          <Text style={styles.viewChatRec}>
-            Chat
-          </Text>
-        </View>
+        <FlatList
+          data={DATA}
+          renderItem={({ item }) => <Item title={item.title} img_url={item.img_url} shop={item.shop} />} // Truyền cả img_url
+          keyExtractor={item => item.id}
+        />
       </View>
-      <View style={styles.content}>
-        <Image source={gaBoToi} style={styles.IMGContent}/>
-        <View>
-          <Text>1KG KHÔ GÀ BƠ TỎI</Text>
-          <Text style={{color:'#F31111'}}>Shop LDTFood</Text>
-        </View>
-        <View>
-          <Image source={rec1} style={styles.chatRec}/>
-          <Text style={styles.viewChatRec}>
-            Chat
-          </Text>
-        </View>
-      </View>
-      <View style={styles.content}>
-        <Image source={xeCanCau} style={styles.IMGContent}/>
-        <View>
-          <Text>Xe cần cẩu đa năng</Text>
-          <Text style={{color:'#F31111'}}>Shop Thế giới đồ chơi</Text>
-        </View>
-        <View>
-          <Image source={rec1} style={styles.chatRec}/>
-          <Text style={styles.viewChatRec}>
-            Chat
-          </Text>
-        </View>
-      </View>
-      <View style={styles.content}>
-        <Image source={doChoiMH} style={styles.IMGContent}/>
-        <View>
-          <Text>Đồ chơi dạng mô hình</Text>
-          <Text style={{color:'#F31111'}}>Shop Thế giới đồ chơi</Text>
-        </View>
-        <View>
-          <Image source={rec1} style={styles.chatRec}/>
-          <Text style={styles.viewChatRec}>
-            Chat
-          </Text>
-        </View>
-      </View>
-      <View style={styles.content}>
-        <Image source={lanhDaoGianDon} style={styles.IMGContent}/>
-        <View>
-          <Text>Lãnh đạo giản đơn</Text>
-          <Text style={{color:'#F31111'}}>Shop Devang</Text>
-        </View>
-        <View>
-          <Image source={rec1} style={styles.chatRec}/>
-          <Text style={styles.viewChatRec}>
-            Chat
-          </Text>
-        </View>
-      </View>
-      <View style={styles.content}>
-        <Image source={hieuLongConTre} style={styles.IMGContent}/>
-        <View>
-          <Text>Hiểu lòng con trẻ</Text>
-          <Text style={{color:'#F31111'}}>Shop Devang</Text>
-        </View>
-        <View>
-          <Image source={rec1} style={styles.chatRec}/>
-          <Text style={styles.viewChatRec}>
-            Chat
-          </Text>
-        </View>
-      </View>
-      
     </View>
   );
 }
